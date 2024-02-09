88b61aa8e7b5249ea29131924d99a96e8f3414e3
Benefrancis <benefrancis@gmail.com>
2024-02-09 07:50:09 -0300

    Initial commit

---
+++ src/main/java/br/com/benefrancis/Main.java
@@ -0,0 +1,30 @@
+package br.com.benefrancis;
+
+import br.com.benefrancis.domain.entity.Pizzaria;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory( "fiap" );
+
+        EntityManager manager = factory.createEntityManager();
+
+
+        Pizzaria p1 = new Pizzaria(null, "Benezinho Pizzaria");
+
+
+
+        manager.getTransaction().begin();
+        manager.persist( p1 );
+        manager.getTransaction().commit();
+
+
+        manager.close();
+        factory.close();
+
+
+    }
+}
---
+++ src/main/java/br/com/benefrancis/domain/entity/Pizzaria.java
@@ -0,0 +1,49 @@
+package br.com.benefrancis.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Pizzaria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PIZZARIA")
+    @SequenceGenerator( name = "SQ_PIZZARIA")
+    private Long id;
+
+    private String nome;
+
+    public Pizzaria() {
+    }
+
+    public Pizzaria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Pizzaria setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Pizzaria setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pizzaria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
