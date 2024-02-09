7dc6ac3d3b3e3453a1455f4ee795df65cfbd9c0f
Benefrancis <benefrancis@gmail.com>
2024-02-09 09:38:13 -0300

    Criamos a entidade Sabor.

--- src/main/java/br/com/benefrancis/Main.java
+++ src/main/java/br/com/benefrancis/Main.java
@@ -1,30 +1,34 @@
 package br.com.benefrancis;
 
 import br.com.benefrancis.domain.entity.Pizzaria;
+import br.com.benefrancis.domain.entity.Sabor;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Main {
+
     public static void main(String[] args) {
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory( "maria-db" );
-
         EntityManager manager = factory.createEntityManager();
 
+        var manjericao = new Sabor( null, "Manjericao", "Deliciosa pizza de manjericão que fora plantado pelos mais renomados agricultores do Brasil" );
 
-        Pizzaria p1 = new Pizzaria(null, "Benezinho Pizzaria");
 
+        Pizzaria p1 = new Pizzaria( null, "Dominus" );
 
 
         manager.getTransaction().begin();
+        manager.persist( manjericao );
         manager.persist( p1 );
         manager.getTransaction().commit();
 
+        System.out.println("PIZZARIA: " + p1);
+
+        System.out.println("SABOR: " + manjericao);
 
         manager.close();
         factory.close();
-
-
     }
 }
